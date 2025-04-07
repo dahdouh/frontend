be96817e1d8e24a8b0e64535d17f5ba54f3bc5da
dahdouh <karim.dahdouh.fr@gmail.com>
2025-04-07 16:49:16 +0200

    feat: add guards

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import { inject } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export const authGuard: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot,
+) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  if(localStorage.getItem('user-data-store')) {
+    return true;
+  }
+  if (!authService.isLoggedIn) {
+    router.navigateByUrl('/');
+  }
+  return authService.isLoggedIn;
+};
